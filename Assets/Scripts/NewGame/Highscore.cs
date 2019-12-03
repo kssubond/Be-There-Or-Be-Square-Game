d47ebbcd608116678cfb1e2d7d1415ef2f631a36
kssubond <kssubond@gmail.com>
2019-12-03 19:50:14 +0200

    Add all assets for the game

---
+++ Assets/Scripts/NewGame/Highscore.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Highscore : MonoBehaviour
+{
+    public Text score;
+
+    void OnEnable()
+    {
+        score.GetComponent<Text>();
+        score.text = "Highscore: " + PlayerPrefs.GetInt("Highscore").ToString();
+    }
+}
---
+++ Assets/Scripts/NewGame/PipePool.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipePool : MonoBehaviour
+{
+    public GameObject pipePrefab;
+    public int pipePoolSize = 5;
+    public float spawnRate = 3f;
+    public float pipeMin = -1f;
+    public float pipeMax = 3.5f;
+
+    private GameObject[] pipes;
+    private int currentPipe = 0;
+
+    private Vector2 objectPoolPosition = new Vector2 (-15, -25);
+
+    private float spawnXPosition = 4f;
+    private float timeSinceLastSpawned;
+
+    void Start()
+    {
+        timeSinceLastSpawned = 0;
+        pipes = new GameObject[pipePoolSize];
+        for (int i = 0; i < pipePoolSize; i++)
+        {
+            pipes[i] = (GameObject)Instantiate(pipePrefab, objectPoolPosition, Quaternion.identity);
+        }
+    }
+
+    void Update()
+    {
+        timeSinceLastSpawned += Time.deltaTime;
+
+        if(GameControl.instance.gameOver == false && timeSinceLastSpawned >= spawnRate)
+        {
+            timeSinceLastSpawned = 0f;
+            float spawnYPosition = Random.Range(pipeMin, pipeMax);
+            pipes[currentPipe].transform.position = new Vector2(spawnXPosition, spawnYPosition);
+            currentPipe++;
+
+            if(currentPipe >= pipePoolSize)
+            {
+                currentPipe = 0;
+            }
+        }
+    }
+}
