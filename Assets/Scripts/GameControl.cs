1f8e11b4ed3f1745c7f06386cefa70773dab5e62
kssubond <kssubond@gmail.com>
2019-12-23 20:02:08 +0200

    Fix player boundaries

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameControl : MonoBehaviour
+{
+    public static GameControl instance;
+
+    public AudioSource scoreSound;
+    public GameObject gameOverPanel;
+    public GameObject startText;
+    public GameObject highscore;
+
+    public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
+    public Text scoreText;
+
+    private int score = 0;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        Pause();
+    }
+
+    void Update()
+    {
+        UnPause();
+    }
+    void Pause()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void UnPause()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Time.timeScale = 1;
+            startText.SetActive(false);
+            highscore.SetActive(false);
+        }
+    }
+
+    public void BirdScored()
+    {
+        if (gameOver)
+            return;
+
+        score++;
+        scoreSound.Play();
+        scoreText.text = score.ToString();
+    }
+
+    public void BirdDied()
+    {
+        gameOverPanel.SetActive(true);
+        gameOver = true;
+        int savedScore = PlayerPrefs.GetInt("Highscore");
+
+        if(score > savedScore)
+        {
+            PlayerPrefs.SetInt("Highscore", score);
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/PipePool.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipePool : MonoBehaviour
+{
+    public GameObject pipePrefab;
+    public int pipePoolSize = 5;
+    public float spawnRate = 3f;
+    public float pipeMin = -1f;
+    public float pipeMax = 3.5f;
+    public float screenOffset = 0.5f;
+
+    private GameObject[] pipes;
+    private int currentPipe = 0;
+
+    private Vector2 objectPoolPosition = new Vector2 (-15, -25);
+
+    private float timeSinceLastSpawned;
+
+    [SerializeField]private new Camera camera;
+
+    void Awake()
+    {
+        
+    }
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+
+        timeSinceLastSpawned = 0;
+        pipes = new GameObject[pipePoolSize];
+
+        for (int i = 0; i < pipePoolSize; i++)
+        {
+            pipes[i] = Instantiate(pipePrefab, objectPoolPosition, Quaternion.identity);
+        }
+    }
+
+    void Update()
+    {
+        camera = Camera.main;
+        Vector3 bottomLeftWorld = camera.ScreenToWorldPoint(new Vector3(0, 0, camera.nearClipPlane));
+        Vector3 topRightWorld = camera.ScreenToWorldPoint(new Vector3(1, 1, camera.nearClipPlane));
+        timeSinceLastSpawned += Time.deltaTime;
+
+        if(GameControl.instance.gameOver == false && timeSinceLastSpawned >= spawnRate)
+        {
+            timeSinceLastSpawned = 0f;
+            float spawnYPosition = Random.Range(pipeMin, pipeMax);
+            pipes[currentPipe].transform.position = new Vector3(Random.Range(-bottomLeftWorld.x, -topRightWorld.x) + screenOffset, spawnYPosition, 0);
+            currentPipe++;
+
+            if(currentPipe >= pipePoolSize)
+            {
+                currentPipe = 0;
+            }
+        }
+    }
+}
